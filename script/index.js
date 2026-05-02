dec0d39c75d9ee824f1e26590085d560d6c06788
Redapycode <redanha01@gmail.com>
2026-05-02 03:51:55 +0100

    modified

---
+++ script/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+var menuItems = document.getElementById("navLinks");
+    if (menuItems.style.display === "block") {
+    menuItems.style.display = "none";
+    } else {
+    menuItems.style.display = "block";
+    }
+}
--- script/login.js
+++ script/login.js
@@ -1,4 +1,15 @@
+function toggle() {
+    let login = document.getElementById("loginBox");
+    let signup = document.getElementById("signupBox");
 
+    if (login.style.display === "none") {
+        login.style.display = "block";
+        signup.style.display = "none";
+    } else {
+        login.style.display = "none";
+        signup.style.display = "block";
+    }
+}
 window.onload = function () {
     const params = new URLSearchParams(window.location.search);
     const mode = params.get("mode");
